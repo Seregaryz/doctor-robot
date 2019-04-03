4215fec7a90820ab0b03f0ab2923af1554109f49
Timur <timurbikmullin@mail.ru>
2019-04-03 03:00:51 +0300

    add url and view offices

--- untitled3/clinic/views.py
+++ untitled3/clinic/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
-# Create your views here.
+from .models import Client, Doctor
+
+
+def home_page(request):
+	return render()
+	
+	
+def doctor_office(request,doctor_id):
+    doctor = get_object_or_404(Doctor, pk=doctor_id)
+	return render(request,'clinic/doctor_office/doctor.html', {'doctor': doctor})
+	
+	
+def patient_office(request,patient_id):
+    patient = get_object_or_404(Client, pk=patient_id)
+	return render(request,'clinic/patient_office/patient.html', {'patient': patient})
