801fd1dd6c7f56943e6c3cf84f5b9e39ec542662
Mikhail Kilin <1kilinmv1@gmail.com>
2019-04-13 12:25:28 +0300

    renamed dir

--- doctorRobot/clinic/admin.py
+++ doctorRobot/clinic/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from clinic.models import Result,UserProfile,DoctorUser
+from clinic.models import Result, UserProfile, DoctorUser
 
 admin.site.register(Result)
 admin.site.register(UserProfile)
